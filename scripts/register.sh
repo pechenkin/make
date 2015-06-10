ba94366d53f4bc510cd87a4a21c2800628c22b41
pechenkin <nikita_pechenkin@relex.ru>
2015-06-10 16:17:23 +0300

    fixup

--- scripts/register.sh
+++ scripts/register.sh
@@ -5,5 +5,5 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     do
         url_key=$(echo $path_key | sed 's/\.path/.url/')
         url=$(git config -f .gitmodules --get "$url_key")
-        git submodule --force add $url $path
+        git submodule add --force $url $path
     done
